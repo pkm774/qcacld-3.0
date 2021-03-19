9ec556452424a26351e6175127d3f5f786a302aa
Abhishek Ambure <aambure@codeaurora.org>
2021-03-19 16:27:33 +0530

    qcacld-3.0: Add ini to enable sae auth/assoc retry
    Introduce new INI sae_connect_retries to decide auth (initial connect
    and roam auth) and connect retry for sae. MAX reties is capped to 2
    and max for roam auth is 1.
    
    Default is 0x49 i.e. 1 retry each.
    Bits       Retry Type
    BIT[0:2]   AUTH retries
    BIT[3:5]   Connection reties
    BIT[6:8]   ROAM AUTH retries
    
    Change-Id: Idae86a7bdb32244c7f3a140b8591418b8238758a
    CRs-Fixed: 2728458

--- core/sme/inc/csr_api.h
+++ core/sme/inc/csr_api.h
@@ -1358,6 +1358,9 @@ typedef struct tagCsrConfigParam {
 	bool enable_pending_list_req;
 	uint32_t disable_4way_hs_offload;
 	uint32_t sta_disable_roam;
+#ifdef WLAN_FEATURE_SAE
+	uint32_t sae_connect_retries;
+#endif
 } tCsrConfigParam;
 
 /* Tush */
