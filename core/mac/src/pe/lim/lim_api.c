eqs-development/android_kernel_motorola_sm8475-modules

qcacld-3.0: Update rssi and seqno as part of roam sync indication
Customer tries to trigger quick connect (connection without scan)
just after roaming and reading scan_cache_entry parameters. But
currently the scan_cache_entry parameters followed by roam sync
event are not filled properly.

To Fix this fill Rssi from roam sync indication fixed parameters
and fill seqno from frame mac header during roam sync indication.

Change-Id: Iebe817ac996ecca3998bcf25e62304797a70ea2b
CRs-Fixed: 2870742

qcom/opensource/wlan/qcacld-3.0/core/mac/src/pe/lim/lim_api.c
@@ -2106,6 +2106,7 @@ lim_roam_fill_bss_descr(struct mac_context *mac,
 				sizeof(bss_desc_ptr->length) + ie_len);
 
 	bss_desc_ptr->fProbeRsp = !roam_synch_ind_ptr->isBeacon;
+	bss_desc_ptr->rssi = roam_synch_ind_ptr->rssi;
 	/* Copy Timestamp */
 	bss_desc_ptr->scansystimensec = qdf_get_monotonic_boottime_ns();
 	if (parsed_frm_ptr->he_op.oper_info_6g_present) {
@@ -2142,6 +2143,11 @@ lim_roam_fill_bss_descr(struct mac_context *mac,
 	qdf_mem_copy((uint8_t *) &bss_desc_ptr->bssId,
 		     (uint8_t *)roam_synch_ind_ptr->bssid.bytes,
 		     sizeof(tSirMacAddr));
+
+	qdf_mem_copy((uint8_t *)&bss_desc_ptr->seq_ctrl,
+		     (uint8_t *)&mac_hdr->seqControl,
+		     sizeof(tSirMacSeqCtl));
+
 	bss_desc_ptr->received_time =
 		      (uint64_t)qdf_mc_timer_get_system_time();
 	if (parsed_frm_ptr->mdiePresent) {

pkm774/qcacld-3.0

core/mac/src/pe/lim/lim_api.c
@@ -2232,6 +2232,7 @@ lim_roam_fill_bss_descr(tpAniSirGlobal pMac,
 				sizeof(bss_desc_ptr->length) + ie_len);
 
 	bss_desc_ptr->fProbeRsp = !roam_offload_synch_ind_ptr->isBeacon;
+	bss_desc_ptr->rssi = roam_offload_synch_ind_ptr->rssi;
 	/* Copy Timestamp */
 	bss_desc_ptr->scansystimensec = qdf_get_monotonic_boottime_ns();
 	if (parsed_frm_ptr->dsParamsPresent) {
@@ -2274,6 +2275,10 @@ lim_roam_fill_bss_descr(tpAniSirGlobal pMac,
 	qdf_mem_copy((uint8_t *) &bss_desc_ptr->bssId,
 		     (uint8_t *) mac_hdr->bssId,
 		     sizeof(tSirMacAddr));
+
+	qdf_mem_copy((uint8_t *)&bss_desc_ptr->seq_ctrl,
+		     (uint8_t *)&mac_hdr->seqControl,
+		     sizeof(tSirMacSeqCtl));
 
 	bss_desc_ptr->received_time =
 		      (uint64_t)qdf_mc_timer_get_system_time();